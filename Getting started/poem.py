8bfd0f69f046eb2370c3c6ad7c89c23c5e66d713
Shubham Singh Darmwal <62296226+shubhamdarmwal@users.noreply.github.com>
2025-04-11 11:39:31 +0530

    Create poem.py

---
+++ Getting started/poem.py
@@ -0,0 +1,13 @@
+"""
+program which prints out the following lines:
+Row, row, row your boat,
+Gently down the stream.
+Merrily, merrily, merrily, merrily,
+Life is but a dream.
+"""
+
+# solution
+print("Row, row, row your boat,")
+print("Gently down the stream.")
+print("Merrily, merrily, merrily, merrily,")
+print("Life is but a dream.")
