59b2bffe6be0b59727a47c963c5b0a2a3dfeb96c
Shubham Singh Darmwal <62296226+shubhamdarmwal@users.noreply.github.com>
2025-04-11 11:34:06 +0530

    Create brothersnames.py

---
+++ Getting started/brothersnames.py
@@ -0,0 +1,8 @@
+# This program is to print out the names of the brothers in alphabetical order
+print("Aapo")
+print("Eero")
+print("Juhani")
+print("Lauri")
+print("Simeoni")
+print("Timo")
+print("Tuomas")
