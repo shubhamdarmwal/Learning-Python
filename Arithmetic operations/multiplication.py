fc3bd75a4d4d66268e93ae1845d03a9651715c8d
Shubham Singh Darmwal <62296226+shubhamdarmwal@users.noreply.github.com>
2025-04-11 12:14:06 +0530

    Create multiplication.py

---
+++ Arithmetic operations/multiplication.py
@@ -0,0 +1,14 @@
+"""
+Please write a program which asks the user for a number. The program then prints out the number multiplied by five.
+
+The program should function as follows:
+
+Please type in a number: 3
+3 times 5 is 15
+"""
+
+#Solution
+num = int(input("Please type in a number:"))
+sol = num*5
+
+print(f"{num} times 5 is {sol}")
