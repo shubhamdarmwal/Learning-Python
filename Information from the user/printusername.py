d8052918d91b2a02de3a279ff85265089b58a526
Shubham Singh Darmwal <62296226+shubhamdarmwal@users.noreply.github.com>
2025-04-11 11:48:03 +0530

    Create printusername.py

---
+++ Information from the user/printusername.py
@@ -0,0 +1,8 @@
+"""
+Please write a program which asks for the user's name and then prints it twice, on two consecutive lines.
+"""
+
+#Solution
+name = input("What is your name?")
+print(name)
+print(name)
