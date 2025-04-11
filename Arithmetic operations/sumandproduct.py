c0632ea88f29924ecf4b66e6db83b9f356bf2a0f
Shubham Singh Darmwal <62296226+shubhamdarmwal@users.noreply.github.com>
2025-04-11 12:25:45 +0530

    Create sumandproduct.py

---
+++ Arithmetic operations/sumandproduct.py
@@ -0,0 +1,20 @@
+"""
+Please write a program which asks the user for two numbers. The program will then print out the sum and the product of the two numbers.
+
+The program should function as follows:
+
+Number 1: 3
+Number 2: 7
+The sum of the numbers: 10
+The product of the numbers: 21
+"""
+
+#Solution
+number1 = int(input("Number 1:"))
+number2 = int(input("Number 2:"))
+
+sum = number1 + number2
+product = number1 * number2
+
+print("The sum of the numbers:", sum)
+print("The product of the numbers:", product)
