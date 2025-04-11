612eb6737695db26c93959d6f31a5918c8dc8700
Shubham Singh Darmwal <62296226+shubhamdarmwal@users.noreply.github.com>
2025-04-11 12:17:47 +0530

    Create secondsinaday.py

---
+++ Arithmetic operations/secondsinaday.py
@@ -0,0 +1,15 @@
+"""
+Please write a program which asks the user for a number of days. The program then prints out the number of seconds in the amount of days given.
+
+The program should function as follows:
+How many days? 1
+Seconds in that many days: 86400
+
+Another example:
+How many days? 7
+Seconds in that many days: 604800
+"""
+
+#Solution
+sec = int(input("How many days?"))
+print(f"Seconds in that many days: {sec * 24 * 60 * 60}")
