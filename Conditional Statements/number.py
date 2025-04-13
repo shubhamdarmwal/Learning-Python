ce47d09499887ed533b572430674e32fba9dfc40
Shubham Singh Darmwal <62296226+shubhamdarmwal@users.noreply.github.com>
2025-04-13 10:42:10 +0530

    Create number.py
    A program which prints out different things based on whether the number the user inputs is negative, positive, or equal to zero:

---
+++ Conditional Statements/number.py
@@ -0,0 +1,12 @@
+#A program which prints out different things based on whether the number the user inputs is negative, positive, or equal to zero:
+
+number = int(input("Please type in a number: "))
+
+if number < 0:
+    print("The number is negative.")
+
+if number > 0:
+    print("The number is positive.")
+
+if number == 0:
+  print("The number is zero.")
