75e404144cb410d8765799db4d144c2ccadb1d73
Shubham Singh Darmwal <62296226+shubhamdarmwal@users.noreply.github.com>
2025-04-13 21:43:18 +0530

    Create soup.py

---
+++ Conditional Statements/soup.py
@@ -0,0 +1,20 @@
+"""
+Please write a program which asks for the user's name. If the name is anything but "Jerry", the program then asks for the number of portions and prints out the total cost. The price of a single portion is 5.90.
+
+Two examples of the program's execution:
+
+Please tell me your name: Kramer
+How many portions of soup? 2
+The total cost is 11.8
+Next please!
+
+Please tell me your name: Jerry
+Next please!
+"""
+
+name = input("Please tell me your name:")
+if name != "Jerry" :
+    portions = int(input("How many portions of soup?"))
+    total = 5.90 * portions
+    print("The total cost is", total)
+print("Next please!")
