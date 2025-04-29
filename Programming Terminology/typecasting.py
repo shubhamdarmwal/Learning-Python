e24b4f4d29356eb341b4c70ba629bf4b089b9cab
Shubham Singh Darmwal <62296226+shubhamdarmwal@users.noreply.github.com>
2025-04-29 12:04:44 +0530

    Update typecasting.py

--- Programming Terminology/typecasting.py
+++ Programming Terminology/typecasting.py
@@ -8,5 +8,8 @@ Please type in a number: 1.34
 Integer part: 1
 Decimal part: 0.34
 """
+num = float(input("Please type in a number:"))
+num1 = num - int(num)
 
- 
+print(f"Integer part: {int(num)}")
+print(f"Decimal part: {num1}")
