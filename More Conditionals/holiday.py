723bae22a2c29ee0380c1bd36a1088eb6587cade
Shubham Singh Darmwal <62296226+shubhamdarmwal@users.noreply.github.com>
2025-04-30 19:51:51 +0530

    Create holiday.py

---
+++ More Conditionals/holiday.py
@@ -0,0 +1,14 @@
+"""
+Write a program which shows a specific holiday for a specific date.
+"""
+print("Holiday calendar")
+date = input("What is the date today? ")
+
+if date == "Dec 26":
+    print("It's Boxing Day")
+elif date == "Dec 31":
+    print("It's Hogmanay")
+elif date == "Jan 1":
+    print("It's New Year's Day")
+
+print("Thanks and bye.")
