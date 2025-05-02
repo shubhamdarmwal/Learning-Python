cf7242f1f97dcfd2910c6b922e0a38986bb3e64b
Shubham Singh Darmwal <62296226+shubhamdarmwal@users.noreply.github.com>
2025-05-03 00:53:18 +0530

    Create greatest.py

---
+++ Combining conditions/greatest.py
@@ -0,0 +1,18 @@
+"""
+Write a program that asks the user to type in four numbers. It then works out which of the four is the greatest.
+"""
+n1 = int(input("Number 1: "))
+n2 = int(input("Number 2: "))
+n3 = int(input("Number 3: "))
+n4 = int(input("Number 4: "))
+
+if n1 > n2 and n1 > n3 and n1 > n4:
+    greatest = n1
+elif n2 > n3 and n2 > n4:
+    greatest = n2
+elif n3 > n4:
+    greatest = n3
+else:
+    greatest = n4
+
+print(f" {greatest} is the greatest of the numbers.")
