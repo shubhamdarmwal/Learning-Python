5cb488af7325a9cc3701f26376200b0645cdb894
Shubham Singh Darmwal <62296226+shubhamdarmwal@users.noreply.github.com>
2025-05-15 11:27:47 +0530

    Update leapyear.py

--- Combining conditions/leapyear.py
+++ Combining conditions/leapyear.py
@@ -18,3 +18,10 @@ Sample output
 Please type in a year: 1800
 That year is not a leap year.
 """
+# Write your solution here
+year = int(input("Please type in a year: "))
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("That year is a leap year.")
+else:
+    print("That year is not a leap year.")
