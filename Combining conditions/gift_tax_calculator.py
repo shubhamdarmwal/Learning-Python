9e5dbdf46614afeb692c85d9dfcb6de6614bde74
Shubham Singh Darmwal <62296226+shubhamdarmwal@users.noreply.github.com>
2025-05-17 12:31:07 +0530

    Update gift_tax_calculator.py

--- Combining conditions/gift_tax_calculator.py
+++ Combining conditions/gift_tax_calculator.py
@@ -31,3 +31,27 @@ Sample output
 Value of gift: 27500
 Amount of tax: 1950.0 euros
 """
+gift = int(input("Value of gift:"))
+
+if gift < 5000 :
+    tax = 0
+
+elif gift < 25000 :
+    tax = 100 + (gift - 5000) * 0.08
+
+elif gift < 55000 :
+    tax = 1700 + (gift - 25000) * 0.10
+
+elif gift < 200000 :
+    tax = 4700 + (gift - 55000) * 0.12
+
+elif gift < 1000000 :
+    tax = 22100 + (gift - 200000) * 0.15
+
+else :
+    tax = 142100 + (gift - 1000000) * 0.17
+
+if tax == 0 :
+    print("No tax!")
+else :
+    print(f"Amount of tax: {tax} euros")
