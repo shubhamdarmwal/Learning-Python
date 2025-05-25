b51c965d61bdd5989b2f7a5892172ecc4bb5bb63
Shubham Singh Darmwal <62296226+shubhamdarmwal@users.noreply.github.com>
2025-05-25 23:29:18 +0530

    Create next_leap_year.py

---
+++ Simple Loops/next_leap_year.py
@@ -0,0 +1,11 @@
+"""
+Please write a program which asks the user for a year, and prints out the next leap year.
+
+Sample output
+Year: 2023
+The next leap year after 2023 is 2024
+
+If the user inputs a year which is a leap year (such as 2024), the program should print out the following leap year:
+Year: 2024
+The next leap year after 2024 is 2028
+"""
