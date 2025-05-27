ad6ec0244e5e0cd60845ebb5b994e1b655584b2c
Shubham Singh Darmwal <62296226+shubhamdarmwal@users.noreply.github.com>
2025-05-27 22:31:25 +0530

    Update next_leap_year.py

--- Simple Loops/next_leap_year.py
+++ Simple Loops/next_leap_year.py
@@ -9,3 +9,15 @@ If the user inputs a year which is a leap year (such as 2024), the program shoul
 Year: 2024
 The next leap year after 2024 is 2028
 """
+start_year = int(input("Year: "))
+year = start_year + 1
+while True:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            break
+    elif year % 4 == 0:
+        break
+ 
+    year += 1
+ 
+print(f"The next leap year after {start_year} is {year}")
